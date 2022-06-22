a98c726ef494dfd3707975e47b4b753fc9a674c8
Robin <8747034+Admentus64@users.noreply.github.com>
2022-06-22 20:46:48 +0200

    Remove unneeded

--- ASM/c/arrow_toggling.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARROW_TOGGLING_H
-#define ARROW_TOGGLING_H
-
-#include "z64.h"
-
-void toggle_arrow();
-void draw_arrow_icon(z64_disp_buf_t *db, uint16_t alpha, uint16_t icon_x, uint16_t icon_y);
-
-#endif
