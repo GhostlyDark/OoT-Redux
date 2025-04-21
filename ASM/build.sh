e7af5ff3478edfb40ec7335cf9c6b8969e706dc6
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2025-04-21 23:05:05 +0200

    build.sh: Fix generating PPF patches

--- ASM/build.sh
+++ ASM/build.sh
@@ -8,7 +8,7 @@ tool_dir="$toplvl_dir/tools"
 
 ./build.py --compile-c
 
-if [[ "$OSTYPE" == "msys"* ]]; then
-    rm -f $rom_dir/redux.ppf
-    cmd //c $tool_dir/MakePPF3 c $rom_dir/base.z64 $rom_dir/patched.z64 $rom_dir/redux.ppf
+if [[ $(uname -s) = MINGW32* ]]; then
+    rm -f "$rom_dir"/redux.ppf
+    "$tool_dir"/MakePPF3 c "$rom_dir"/base.z64 "$rom_dir"/patched.z64 "$rom_dir"/redux.ppf
 fi
